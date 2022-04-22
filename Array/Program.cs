31e438ddffd4cbc64972b777ecb4dde0838454bd
Maeflower05 <annajacobsen1999@hotmail.com>
2022-04-22 09:46:21 +0200

    Add project files.

---
+++ Array/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] value = { 1, 3, 5, 2, 4, 0 };
+string[] abbrev = { "CPU", "RAM", "HDD", "USB", "VGA", "DVI" };
+//foreach (int i in value)
+//{
+//    Console.WriteLine(value[i]);
+//}
+
+for (int i = 0; i < value.Length; i++)
+{
+    Console.WriteLine(abbrev[value[i]]);
+}
+Console.ReadLine();
